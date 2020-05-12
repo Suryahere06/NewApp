dabcdca10600c9355fd18bb2372dc88de6cfc7df
Suryahere06 <58037576+Suryahere06@users.noreply.github.com>
2020-05-12 18:56:51 -0400

    chrome driver path change

--- src/main/java/com/iPivot/DataDrivenApp/Utilities/SelectBrowser.java
+++ src/main/java/com/iPivot/DataDrivenApp/Utilities/SelectBrowser.java
@@ -43,12 +43,12 @@ try {
 
 	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 	ChromeOptions options = new ChromeOptions();
-	options.setBinary("/usr/bin/chrome-browser");
+	options.setBinary("/usr/bin/chromium-browser");
 
-	options.addArguments("start-maximized"); // open Browser in maximized mode
+	//options.addArguments("start-maximized"); // open Browser in maximized mode
 	options.addArguments("disable-infobars"); // disabling infobars
 	options.addArguments("--disable-extensions"); // disabling extensions
-	options.addArguments("--disable-gpu"); // applicable to windows os only
+	options.addArguments("--headless"); // applicable to windows os only
 	options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 	options.addArguments("--no-sandbox	"); // Bypass OS security model
 
